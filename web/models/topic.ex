cb184d50b0825b166eaef31b0a8185c154bd2331
Zephyr Worthington <z.worthington@outlook.com>
2020-01-10 12:47:06 -0800

    Lesson 73: generating changesets for the form

--- web/models/topic.ex
+++ web/models/topic.ex
@@ -1,8 +1,19 @@
 defmodule Discuss.Topic do
-    user Discuss.Web, :model
+    use Discuss.Web, :model
 
 
     schema "topics" do
         field :title, :string
+    end
+
+# changeset tells us how we want to update the database
+# Also describes any validation errors that might occur
+# take changeset from cast and send to however many validators are defined
+# eventually returns a new changeset after all validators
+# final result represents how we're trying to update our database
+    def changeset(struct, params \\ %{}) do
+        struct
+        |> cast(params, [:title])
+        |> validate_required([:title])
     end
 end
