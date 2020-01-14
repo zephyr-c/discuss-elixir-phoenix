77cc9ac9f8204828f025b847b18962ce6945f2a8
Zephyr Worthington <z.worthington@outlook.com>
2020-01-13 21:21:43 -0800

    Lesson 90: Linking to the Edit Topic Form

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -36,7 +36,8 @@ defmodule Discuss.TopicController do
     end
 
     def update(conn, %{"id" => topic_id, "topic" => topic}) do
-        changeset = Repo.get(Topic, topic_id) |> Topic.changeset(topic)
+        old_topic = Repo.get(Topic, topic_id)
+        changeset = Topic.changeset(old_topic, topic)
 
         case Repo.update(changeset) do
             {:ok, _topic} ->
@@ -44,7 +45,7 @@ defmodule Discuss.TopicController do
                 |> put_flash(:info, "Topic Updated")
                 |> redirect(to: topic_path(conn, :index))
             {:error, changeset} ->
-                render conn, "edit.html", changeset: changeset
+                render conn, "edit.html", changeset: changeset, topic: old_topic
         end
     end
 end
